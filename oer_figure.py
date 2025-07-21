54c490e519a7bbe584af7b8d9c2c88435715c063
Felix Therrien <felix.therrien@gmail.com>
2025-07-21 17:25:28 -0400

    Minimal modifications to make it work with new names

--- oer_figure.py
+++ oer_figure.py
@@ -1,20 +1,13 @@
-#adE calculated with paper element values, with Gcorr
-#modified code to search through structure
-import numpy as np
 import pandas as pd
-import re
 import matplotlib.pyplot as plt
-from sklearn.linear_model import LinearRegression
-import newPlots
 import seaborn as sns
-import oer_functions2
-import matplotlib.gridspec as gridspec
+import oc_analyzer.oc2022.oer_utils as oer_functions2
 
-filename = 'sortedWithAdE.csv' #all adsorbates: 31425
+filename = 'data/oc2022/adsorption_energies.csv' #all adsorbates: 31425
 adE = pd.read_csv(filename, index_col=0, na_values='')
-adE = adE.dropna() #31425
-adE.drop(['traj_id', 'y_relaxed', 'natoms', 'nads2'], axis=1, inplace=True)
-adE.reset_index(inplace=True)
+#adE = adE.dropna() #31425
+#adE.drop(['traj_id', 'y_relaxed', 'natoms', 'nads2'], axis=1, inplace=True)
+#adE.reset_index(inplace=True)
 
 mask = (adE['ads_symbols'] == 'H2O') | (adE['ads_symbols'] == 'OH') | (adE['ads_symbols'] == 'O') | (adE['ads_symbols'] == 'HO2') | (adE['ads_symbols'] == 'O2') 
 adE2 = adE[mask]  #no correction to O, OH, OOH at this point, 19255
@@ -27,7 +20,7 @@ hue_order =['H2O', 'OH', 'O', 'HO2',  'O2']
 
 del adE3['adsorption_energy']
 adE3 = adE3.rename(columns={'corrected_adsorption_energy': 'adsorption_energy'})
-adE3.to_csv('correctedAdE.csv')
+#adE3.to_csv('correctedAdE.csv')
 
 #distributions
 #supporting figures
@@ -50,22 +43,21 @@ ax.set_ylabel('Adsorption Energy (eV)', fontsize=32, fontweight='bold')
 plt.savefig("my_figure.png", dpi=300, bbox_inches='tight')
 plt.show()
 
-#ranges
-adE_OH = oer_functions2.maskAndSort(adE3, 'OH') 
-print(len(adE_OH), adE_OH['adsorption_energy'].min(), adE_OH['adsorption_energy'].max(), adE_OH['adsorption_energy'].mean())
-adE_O = oer_functions2.maskAndSort(adE3, 'O') 
-print(len(adE_O), adE_O['adsorption_energy'].min(), adE_O['adsorption_energy'].max(), adE_O['adsorption_energy'].mean())
-adE_HO2 = oer_functions2.maskAndSort(adE3, 'HO2') 
-print(len(adE_HO2), adE_HO2['adsorption_energy'].min(), adE_HO2['adsorption_energy'].max(), adE_HO2['adsorption_energy'].mean())
+# #ranges
+# adE_OH = oer_functions2.maskAndSort(adE3, 'OH') 
+# print(len(adE_OH), adE_OH['adsorption_energy'].min(), adE_OH['adsorption_energy'].max(), adE_OH['adsorption_energy'].mean())
+# adE_O = oer_functions2.maskAndSort(adE3, 'O') 
+# print(len(adE_O), adE_O['adsorption_energy'].min(), adE_O['adsorption_energy'].max(), adE_O['adsorption_energy'].mean())
+# adE_HO2 = oer_functions2.maskAndSort(adE3, 'HO2') 
+# print(len(adE_HO2), adE_HO2['adsorption_energy'].min(), adE_HO2['adsorption_energy'].max(), adE_HO2['adsorption_energy'].mean())
 
 #merged dataframe with surfaces with all 3 adsorbates: OH, O, HO2
 df_oer = oer_functions2.mergeThree(adE3, 'OH', 'O', 'HO2')
-breakpoint()
 
-#range
-print('OH: ', df_oer['OH'].min(), df_oer['OH'].max(), df_oer['OH'].mean())
-print('O: ', df_oer['O'].min(), df_oer['O'].max(), df_oer['O'].mean())
-print('OOH: ', df_oer['HO2'].min(), df_oer['HO2'].max(), df_oer['HO2'].mean())
+# #range
+# print('OH: ', df_oer['OH'].min(), df_oer['OH'].max(), df_oer['OH'].mean())
+# print('O: ', df_oer['O'].min(), df_oer['O'].max(), df_oer['O'].mean())
+# print('OOH: ', df_oer['HO2'].min(), df_oer['HO2'].max(), df_oer['HO2'].mean())
 
 #THERMODYNAMIC OVERPOTENTIAL
 df_oer = oer_functions2.reaction_energies(df_oer)
@@ -73,27 +65,29 @@ df_oer['maxG'] = df_oer[['delG1', 'delG2', 'delG3', 'delG4']].apply(max, axis=1)
 df_oer['RDS'] = df_oer.apply(oer_functions2.get_rds, axis=1)
 df_oer['eta'] = df_oer['maxG'] - 1.23
 df_oer2 = df_oer.copy().sort_values(by='eta')
-df_oer2.to_csv('all3.csv')
+#df_oer2.to_csv('all3.csv')
 
 oer_functions2.scalingEtaPlot(df_oer, 1.24, 1.64, -0.4)
 plt.savefig("my_figure.png", dpi=300, bbox_inches='tight')
-plt.show()
+#plt.show()
 
-breakpoint()
+
 #plot
 mask = (df_oer['delG2'] >= -5 ) & (df_oer['delG2'] <= 5 )
 df_oer2 = df_oer[mask]
 oer_functions2.plot3(df_oer2, 1.2, 1.5, -0.4)
 #oer_functions.plot4(df_oer, 1.2, 1.5, -0.4)
 
-mask = df_oer['eta'] < 0.4
-good_catalysts = df_oer[mask]
-good_catalysts = good_catalysts.sort_values(by='eta')
-print(good_catalysts[['bulk_symbols', 'bulk_id', 'eta']])
-good_catalysts.to_csv('good.csv')
+plt.show()
 
-mask2 = df_oer['eta'] < 0.5
-good_cat2 = df_oer[mask2]
-good_cat2 = good_cat2.sort_values(by='eta')
-print(good_cat2[['bulk_symbols', 'bulk_id', 'eta']])
-breakpoint()
+# mask = df_oer['eta'] < 0.4
+# good_catalysts = df_oer[mask]
+# good_catalysts = good_catalysts.sort_values(by='eta')
+# print(good_catalysts[['bulk_symbols', 'bulk_id', 'eta']])
+# #good_catalysts.to_csv('good.csv')
+
+# mask2 = df_oer['eta'] < 0.5
+# good_cat2 = df_oer[mask2]
+# good_cat2 = good_cat2.sort_values(by='eta')
+# print(good_cat2[['bulk_symbols', 'bulk_id', 'eta']])
+
