741f4892a95730c983e47f3bbb5106739d70709e
Felix Therrien <felix.therrien@gmail.com>
2025-08-27 15:01:15 -0400

    Automatic creation of the large latex tables

--- similarity_analysis.py
+++ similarity_analysis.py
@@ -28,62 +28,83 @@ def group_similar(df, subset, quantity):
         diffs.append(same_pair[quantity].max() - same_pair[quantity].min())
         dists_to_mean.append(same_pair[quantity] - same_pair[quantity].mean())
         dists_to_median.append(same_pair[quantity] - same_pair[quantity].mean())
-    
-    print("Average square distance with mean (RMSE like): ", np.sqrt(np.mean(np.concatenate(dists_to_mean)**2)))
-    print("Average difference with median (MAE like): ", np.mean(abs(np.concatenate(dists_to_median))))
+
+    rmse = np.sqrt(np.mean(np.concatenate(dists_to_mean)**2))
+    # mae = np.mean(abs(np.concatenate(dists_to_median)))
+    # mean_diff = np.mean(diffs)
+    max_diff = np.max(diffs)
+
+    print("Average square distance with mean (RMSE like): ",
+          np.sqrt(np.mean(np.concatenate(dists_to_mean)**2)))
+    print("Average difference with median (MAE like): ",
+          np.mean(abs(np.concatenate(dists_to_median))))
     print("Average difference between extremes: ", np.mean(diffs))
     print("Maximum difference between extremes: ", np.max(diffs))
 
-def print_stats(data, adsorbate, oc22=True):
-    print(f"{adsorbate} adsorption energies:")
+    return {'$n_{\\text{groups}}$ / $n_{\\text{struc.}}$':
+            f"{(len(repeated_pairs))} / {len(duplicated_df)}",
+            "RMSE$^*$ (eV)": f"{rmse:.2f}",
+            # "MAE$^*$)": mae,
+            # "Mean diff": mean_diff,
+            "Max diff (eV)": f"{max_diff:.2f}"}
+
+
+def print_stats(data, adsorbates, oc22=True):
+    data_dict = {}
+    print(f"{' and '.join(adsorbates)} adsorption energies:")
     print("################################################################")
     print("Same miller (termination + coverage + site)")
     print("--------------------------------------------------------------")
-    group_similar(data[data['ads_symbols'] == adsorbate], subset=['bulk_id', 'miller_index'], quantity='adsorption_energy')
+    data_dict["Same miller (termination + coverage + site)"] = group_similar(data[data['ads_symbols'].isin(adsorbates)], subset=['ads_symbols', 'bulk_id', 'miller_index'], quantity='adsorption_energy')
     print("--------------------------------------------------------------")
     if oc22:
-        print()
-        print("Same slab_sid (coverage + site)")
+        print("Same miller and nads (termination + site)")
         print("--------------------------------------------------------------")
-        group_similar(data[data['ads_symbols'] == adsorbate], subset=['slab_sid'], quantity='adsorption_energy')
+        data_dict["Same miller and $n_\\text{ads}$ (termination + site)"] = group_similar(data[data['ads_symbols'].isin(adsorbates)], subset=['ads_symbols', 'bulk_id', 'miller_index','nads'], quantity='adsorption_energy')
+        print()
+        print("Same slab ID (coverage + site)")
+        print("--------------------------------------------------------------")
+        data_dict["Same slab ID (coverage + site)"] = group_similar(data[data['ads_symbols'].isin(adsorbates)], subset=['ads_symbols', 'slab_sid'], quantity='adsorption_energy')
         print("--------------------------------------------------------------")
         print()
         print("Same slab_sid and nads (site)")
         print("--------------------------------------------------------------")
-        group_similar(data[data['ads_symbols'] == adsorbate], subset=['slab_sid', 'nads'], quantity='adsorption_energy')
+        data_dict["Same slab ID and $n_\\text{ads}$ (site)"] = group_similar(data[data['ads_symbols'].isin(adsorbates)], subset=['ads_symbols', 'slab_sid', 'nads'], quantity='adsorption_energy')
         print("--------------------------------------------------------------")
     print("################################################################")
     print()
-    
+
+    return data_dict
+
 
 if __name__ == "__main__":
     # Load data
+    filter_data = True
     oc22_data = pd.read_csv('data/oc2022/adsorption_energies.csv', index_col=0)
     oc20_data = pd.read_csv('data/oc2020/lmdb+metadata.csv', index_col=0)
 
     oc20_data = oc20_data.rename(columns={"bulk_mpid": "bulk_id"})
 
-    print("OC22 data:")
-    print_stats(oc22_data, adsorbate='OH')
-    print_stats(oc22_data, adsorbate='O')
-    print_stats(oc22_data, adsorbate='HO2')
-    print()
-    print("OC20 data:")
-    print_stats(oc20_data, adsorbate='*H',oc22=False)
-    print()
-    print()
-    
-    print("After filtering out bad adsorbates:")
-    oh_filter = get_oh_filter()
-    ooh_filter = get_ooh_filter()
-    oc22_data = oc22_data.drop(oh_filter[~oh_filter].index)
-    oc22_data = oc22_data.drop(ooh_filter[~ooh_filter].index)
-    oc20_data = oc20_data[oc20_data['anomaly'] == 0]
+    if filter_data:
+        print("Filtering out bad adsorbates:")
+        oh_filter = get_oh_filter()
+        ooh_filter = get_ooh_filter()
+        oc22_data = oc22_data.drop(oh_filter[~oh_filter].index)
+        oc22_data = oc22_data.drop(ooh_filter[~ooh_filter].index)
+        oc20_data = oc20_data[oc20_data['anomaly'] == 0]
 
     print("OC22 data:")
-    print_stats(oc22_data, adsorbate='OH')
-    print_stats(oc22_data, adsorbate='O')
-    print_stats(oc22_data, adsorbate='HO2')
+    oc22_dict = {}
+    oc22_dict["*OH"] = pd.DataFrame(print_stats(oc22_data, adsorbates=['OH'])).T.stack()
+    oc22_dict["*O"] = pd.DataFrame(print_stats(oc22_data, adsorbates=['O'])).T.stack()
+    oc22_dict["*OOH"] = pd.DataFrame(print_stats(oc22_data, adsorbates=['HO2'])).T.stack()
+    oc22_dict["All"] = pd.DataFrame(print_stats(oc22_data, adsorbates=['OH', 'O', 'HO2'])).T.stack()
     print()
+
+    pd.DataFrame(oc22_dict).to_latex("paper/tables/oc22_similarity_stats.tex")
+
     print("OC20 data:")
-    print_stats(oc20_data, adsorbate='*H',oc22=False)
+    oc20_dict = {}
+    oc20_dict["*H"] = pd.DataFrame(print_stats(oc20_data, adsorbates=['*H'],oc22=False)).T.stack()
+
+    pd.DataFrame(oc20_dict).to_latex("paper/tables/oc20_similarity_stats.tex")
